434db73b0191e6baae409a44f82806ed819812f5
Agah Güler <guleragah@gmail.com>
2023-08-05 22:00:33 +0300

    Context sınıfı olusturuldu

---
+++ src/AgLeather.Shop.Persistance/Context/AgLeatherContext.cs
@@ -0,0 +1,68 @@
+﻿using AgLeather.Shop.Domain.Common;
+using AgLeather.Shop.Domain.Entities;
+using AgLeather.Shop.Persistance.Mappings;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgLeather.Shop.Persistance.Context
+{
+    public class AgLeatherContext : DbContext
+    {
+        #region DbSet
+        public AgLeatherContext(DbContextOptions<AgLeatherContext> options) : base(options) { }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+
+        #endregion
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AccountMapping());
+            modelBuilder.ApplyConfiguration(new AddressMapping());
+            modelBuilder.ApplyConfiguration(new CategoryMapping());
+            modelBuilder.ApplyConfiguration(new CityMapping());
+            modelBuilder.ApplyConfiguration(new CommentMapping());
+            modelBuilder.ApplyConfiguration(new CustomerMapping());
+            modelBuilder.ApplyConfiguration(new OrderMapping());
+            modelBuilder.ApplyConfiguration(new OrderDetailMapping());
+            modelBuilder.ApplyConfiguration(new ProductMapping());
+            modelBuilder.ApplyConfiguration(new ProductImageMapping());
+        }
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity>().ToList())
+                switch (entry.State)
+                {
+                    //update
+                    case EntityState.Modified:
+                        entry.Entity.ModifiedDate = DateTime.Now;
+                        entry.Entity.ModifiedBy = "admin";
+                        break;
+
+                    case EntityState.Added:
+                        entry.Entity.CreateDate = DateTime.Now;
+                        entry.Entity.CreateBy = "admin";
+                        break;
+                    default:
+                        break;
+
+                }
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+        
+    }
+
+
+}
